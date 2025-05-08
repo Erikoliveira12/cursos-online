cee37b779b15a209d0f6b3358d731cf55c505e45
Erik Oliveira da Silva <darkerik221@gmail.com>
2025-05-07 21:48:39 -0300

    versao inicial

---
+++ src/test/java/com/belval/Cursos/Online/CursosOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.belval.Cursos.Online;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursosOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
